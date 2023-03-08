b1a582b68d84507dc42c1b86bccdc40c9cdf2133
niemistaru <taru.nieminen@student.laurea.fi>
2023-03-08 17:49:09 +0200

    Reitit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+//GUESTBOOKIN INDEX. IHa vaa jotta muistat
+
 var express = require('express');
 var app = express();
+//app.use(express.static('tähän se kansion nimi'));
 
 app.get('/', function(req, res) {
-    res.send('Cowabunga');
+    res.send('This is lovely');
 });
+app.get('/guestbook', function(req, res) {
+    res.send('Huh huh nimittäin ihan mieletön vieraskirja');
+})
+app.get('/newmessage', function(req, res) {
+    res.send('You have one new message');
+})
+app.get('/ajaxmessage', function(req, res) {
+    res.send('Litti on kingi ja Kluivert on kurko');
+})
 
 app.listen(8081, function() {
     console.log('KGB kuuntelee porttia 8081');
